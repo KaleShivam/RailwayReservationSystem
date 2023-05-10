ec0220815dbc18ad66d622a4ac7aa21ea0045423
ShivamKale <shivamkale44@gmail.com>
2023-05-11 01:13:05 +0530

    Add project files.

---
+++ RailwayReservationSystem/Models/Railwaycontext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RailwayReservationSystem.Models;
+
+namespace RailwayReservationSystem.Data
+{
+    public class RailwayContext : DbContext
+    {
+        public RailwayContext(DbContextOptions<RailwayContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Train> Trains { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+
+    }
+}
